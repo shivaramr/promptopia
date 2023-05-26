3519ce555eef27893f8348d523dbc7b670661c64
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-26 12:07:08 +0530

    layout code formating

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import '@styles/globals.css';
+import React from "react";
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
+import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description:"Discover & Share AI Prompts"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <Provider>
-            <div className='main'>
-                <div className="gradient"/>
-            </div>
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-            </Provider>
-        </body>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
