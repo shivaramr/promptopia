ca7db4aa634d5871476f209595f8fd8827501963
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-31 10:57:29 +0530

    create prompt page code formating

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,53 +1,53 @@
-"use client"
+"use client";
 
-import React,{useState} from 'react'
-import { useSession } from 'next-auth/react'
-import {useRouter} from 'next/navigation';
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Form from '@components/Form';
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-    const router = useRouter();
-    const {data:session} =useSession();
-    const [submitting,setSubmitting] = useState(false);
-    const [post,setPost] = useState({
-        prompt: '',
-        tag: '',
-    });
-    const createPrompt = async (e)=>{
-        e.preventDefault();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const createPrompt = async (e) => {
+    e.preventDefault();
 
-        setSubmitting(true);
+    setSubmitting(true);
 
-        try {
-            const response = await fetch('/api/prompt/new',{
-                method:'POST',
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    userId: session?.user.id,
-                    tag: post.tag
-                })
-            })
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-            if(response.ok){
-                router.push('/')
-            }
-        } catch (error){
-            console.log({error});
-        } finally{
-            setSubmitting(false);
-        }
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log({ error });
+    } finally {
+      setSubmitting(false);
     }
+  };
 
   return (
-    <Form 
-        type="Create"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={createPrompt}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
-  )
-}
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
