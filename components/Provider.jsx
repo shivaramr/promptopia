79420092a96afc8526b96203f03488b79355456f
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-06-03 15:15:46 +0530

    session provider code formating

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,15 +1,11 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider } from "next-auth/react";
 
-const Provider = ({children,session}) => {
-  return (
-    <SessionProvider session={session}>
-      {children}
-    </SessionProvider>
-  )
-}
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
 
-export default Provider
+export default Provider;
